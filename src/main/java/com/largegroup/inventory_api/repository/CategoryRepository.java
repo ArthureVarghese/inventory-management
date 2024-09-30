e045e1c44363aba0bb329b7d5fa5eb96bb7f4873
Arthure A Varghese <56860740+ArthureVarghese@users.noreply.github.com>
2024-09-30 14:46:18 +0530

    Added Repository Support

---
+++ src/main/java/com/largegroup/inventory_api/repository/CategoryRepository.java
@@ -0,0 +1,7 @@
+package com.largegroup.inventory_api.repository;
+
+import com.largegroup.inventory_api.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CategoryRepository extends JpaRepository<Integer, Category> {
+}
