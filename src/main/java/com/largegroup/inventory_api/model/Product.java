bf0d3dd6ef74b0423c9d393f23811da13d12ed83
Pooja Nair P <itspoojanairp@gmail.com>
2024-09-30 11:39:51 +0530

    Added models

--- src/main/java/com/largegroup/inventory_api/model/Product.java
+++ src/main/java/com/largegroup/inventory_api/model/Product.java
@@ -1,4 +1,29 @@
 package com.largegroup.inventory_api.model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.Data;
+
+@Data
+@Entity(name="product")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "category_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private Category category;
+
+    private Double price;
+    
+    private Integer quantity;
 }
